77943d23524437b52d9df156569d7b38f22feb4d
Meredith Cornelius <meredith.cornelius@gmail.com>
2021-07-26 18:54:45 -0400

    multiplicative persistence

--- multiplicative-persistence/problem.md
+++ multiplicative-persistence/problem.md
@@ -2,10 +2,36 @@ PROMPT:
 
 Given a non-negative integer, write a function that returns its multiplicative persistence--the number of times you must multiply the digits in a number together until you reach a single digit product.
 
-Ex: 39 returns 3
-
-3 x 9 = 27
-2 x 7 = 14
-1 x 4 = 4
-
 EXAMPLES:
+
+456 returns 2
+1: 4 x 5 x 6 = 120
+2: 1 x 2 x 0 = 2
+
+6789 returns 2
+1: 6 x 7 x 8 x 9 = 3024
+2: 3 x 0 x 2 x 4 = 0
+
+7878 returns
+1: 7 x 8 x 7 x 8 = 3136
+2: 3 x 1 x 3 x 6 = 54
+3: 5 x 4 = 20
+4: 2 x 0 = 0
+
+39 returns 3
+
+1: 3 x 9 = 27
+2: 2 x 7 = 14
+3: 1 x 4 = 4
+
+45 returns 2
+
+1: 4 x 5 = 20
+2: 2 x 0 = 0
+
+15 returns 1
+
+1: 1 x 5 = 5
+
+Space Complexity: Unknown - no predictable relationship between the size of the input and how many recursive calls that need to be made (though space complexity is O(1) in iterative solution)
+Time Complexity: Unknown - no predictable relationship between the size of the input and how long it takes to solve
---
+++ multiplicative-persistence/solution.js
@@ -0,0 +1,96 @@
+/* ITERATIVE SOLUTION
+if the num is less than or equal to 9, return 0;
+if the num is less than or equal to 24, return 1;
+
+Create a counter
+Declare a current number, set it to num
+
+Declare a while loop that executes as long as the number is greater than 9
+Increment counter by 1 
+If the number is less than or equal to 24, return counter;
+
+
+Otherwise, coerce number into string and split; save array to a variable
+Declare a current total and set it to the first value in the array, coerced into a number
+Iterate over the array, starting at idx 1 and multiply the current total by the element at that index (coerced into a Number)
+
+When the for loop is complete, Set the current number to the current total
+
+When the while loop is complete, return counter
+*/
+
+// const multiPers = (num) => {
+//   if (num <= 9) return 0;
+//   if (num <= 24) return 1;
+
+//   let counter = 0;
+//   let currNum = num;
+
+//   while (currNum > 9) {
+//     counter++;
+//     if (currNum <= 24) {
+//       return counter;
+//     }
+//     let arrayOfNums = String(currNum).split("");
+//     let currTotal = Number(arrayOfNums[0]);
+//     for (let i = 1; i < arrayOfNums.length; i++) {
+//       currTotal *= Number(arrayOfNums[i]);
+//     }
+//     currNum = currTotal;
+//   }
+//   return counter;
+// };
+
+const multiPers = (num) => {
+  if (num <= 9) return 0;
+
+  let counter = 0;
+  let currNum = num;
+
+  while (currNum > 9) {
+    counter++;
+    currNum = String(currNum)
+      .split("")
+      .reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+  }
+  return counter;
+};
+
+console.log(multiPers(24)); //1
+console.log(multiPers(9019823109238192938)); //1
+console.log(multiPers(9111111111111111111111111111111111111)); //1
+console.log(multiPers(456)); //2
+console.log(multiPers(39)); //3
+console.log(multiPers(7878)); //4
+console.log(multiPers(277777788888899)); //11
+
+/* 
+RECURSIVE SOLUTION
+
+Base case: If num is less than or equal to 9, return 0;
+
+Else: 
+For loop, same as iterative solution;
+
+Return 1 + multiPersRec(newNum)
+*/
+
+const multiPersRec = (num) => {
+  if (num <= 9) {
+    return 0;
+  } else if (num <= 24) {
+    return 1;
+  } else {
+    let arrayOfNums = String(num).split("");
+    let currTotal = Number(arrayOfNums[0]);
+    for (let i = 1; i < arrayOfNums.length; i++) {
+      currTotal *= Number(arrayOfNums[i]);
+    }
+    return 1 + multiPersRec(currTotal);
+  }
+};
+
+// console.log(multiPersRec(39)); //3
+// console.log(multiPersRec(24)); //1
+// console.log(multiPersRec(456)); //2
+// console.log(multiPersRec(7878)); //4
